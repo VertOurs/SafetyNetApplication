a8711edb1718f61cc119f30147c56b9356f9cbc4
Ours <vertours.dev@gmail.com>
2021-10-15 15:09:38 +0200

    Classe Controller et repository update
    modification faite avec armel de repository, 0 controller

--- src/main/java/fr/vertours/safetynetapplication/repository/AllergyRepository.java
+++ src/main/java/fr/vertours/safetynetapplication/repository/AllergyRepository.java
@@ -1,8 +1,10 @@
 package fr.vertours.safetynetapplication.repository;
 
+import fr.vertours.safetynetapplication.model.Allergy;
 import fr.vertours.safetynetapplication.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AllergyRepository extends JpaRepository<Person, Long> {
-    
+public interface AllergyRepository extends JpaRepository<Allergy, Long> {
+
+    Allergy findOneByAllergy(String allergyName);
 }
--- src/main/java/fr/vertours/safetynetapplication/repository/MedicationRepository.java
+++ src/main/java/fr/vertours/safetynetapplication/repository/MedicationRepository.java
@@ -1,8 +1,10 @@
 package fr.vertours.safetynetapplication.repository;
 
 import fr.vertours.safetynetapplication.model.MedicalRecord;
+import fr.vertours.safetynetapplication.model.Medication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MedicationRepository extends JpaRepository<MedicalRecord, Long> {
+public interface MedicationRepository extends JpaRepository<Medication, Long> {
 
+    Medication findOneByMedication(String medication);
 }
